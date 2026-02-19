2f6b9fc4f767dd4ae079ab9bf799090a553ea2c7
Marcell Meilinger <marcell.meilinger@gmail.com>
2026-02-19 12:33:56 +0100

    Hibak javitasa

--- main.cpp
+++ main.cpp
@@ -4,44 +4,32 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    //Elgépelt változónév. N_ELEMENTS helyett NELEMENTS (hiányzik az aláhúzás)
-    int *b = new int[NELEMENTS]; 
+    int *b = new int[N_ELEMENTS];
     
-    //A karakterláncot kettős idézőjelbe kell tenni, nem szimplába.
-    //Hiányzik a pontosvessző a sor végéről.
-    std::cout << '1-100 ertekek duplazasa' 
+    std::cout << "1-100 ertekek duplazasa" << std::endl;
     
-    //A for ciklus hiányos. Hiányzik a feltétel és az iterációs lépés (i++).
-    //i < N_ELEMENTS feltétel nélkül túlmehet a tömb határain.
-    for (int i = 0;) 
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
         b[i] = i * 2;
     }
 
-    //A feltétel csak "i", ami 0-nál azonnal hamis, így a ciklus sosem fut le.
-    //A ciklusmagból hiányzik az adat (b[i]) és a pontosvessző.
-    for (int i = 0; i; i++)
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek: " << b[i] << std::endl;
     }    
-
+    
     std::cout << "Atlag szamitasa: " << std::endl;
     
-    //Az 'atlag' változó nincs inicializálva (0-ra), így szemét értékről indul a számolás.
-    //Az átlaghoz double vagy float kellene, különben az osztásnál elvesznek a tizedesjegyek.
-    int atlag;
-
-    //A for ciklusban vessző (,) van pontosvessző (;) helyett a feltétel után.
-    for (int i = 0; i < N_ELEMENTS, i++)
+    double atlag = 0;
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
-        //Hiányzik a pontosvessző a sor végéről.
-        atlag += b[i]
+        atlag += b[i];
     }
 
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
 
-    //Memóriaszivárgás! A 'new'-val lefoglalt tömb nincs felszabadítva (delete[] b;).
+    delete[] b;
 
     return 0;
 }
